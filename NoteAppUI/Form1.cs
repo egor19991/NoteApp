6e24012dd9b5249040eb30749c68b3881c54f4df
egorkrasnov1999 <egorkrasnov1999@yandex.ru>
2020-10-02 11:26:13 +0700

    comment test

--- NoteAppUI/Form1.cs
+++ NoteAppUI/Form1.cs
@@ -16,5 +16,10 @@ namespace NoteAppUI
         {
             InitializeComponent();
         }
+        //comment
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
