7c47d51e35fb64bea8c424715374d200d667c357
egorkrasnov1999 <egorkrasnov1999@yandex.ru>
2020-10-25 21:40:03 +0700

    Созданы поля для заметок

--- NoteApp/Note.cs
+++ NoteApp/Note.cs
@@ -1,8 +1,40 @@
 ﻿using System;
 
-public class Class1
+namespace NoteApp
 {
-	public Class1()
-	{
-	}
+    public class Note
+    {
+        /// <summary>
+        /// Возвращает и задает название.
+        /// </summary>
+        public string Title 
+        {
+            get {};
+            set
+            {
+                if (value.Length > 50)
+                {
+                    
+                }
+            }
+        }
+        /// <summary>
+        /// Возвращает и задает категорию заметки.
+        /// </summary>
+        public string Category { get; set; }
+        /// <summary>
+        /// Возвращает и задает текст заметки.
+        /// </summary>
+        public string Text { get; set; }
+        /// <summary>
+        /// Возвращает и задает время создания заметки.
+        /// </summary>
+        protected string TimeCreate { get; set; }
+        /// <summary>
+        /// Метод, выполняющий смену даты изменения.
+        /// </summary>
+        public string TimeChange { get; set; }
+    }
+
 }
+
