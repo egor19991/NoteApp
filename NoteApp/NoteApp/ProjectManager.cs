4fdaae7ab3d1de22ed870c567ace4470d4633ab6
egorkrasnov1999 <egorkrasnov1999@yandex.ru>
2020-11-26 14:52:46 +0700

    Добавлена сериализаиция и десериализаиция в ProjectManajer

--- NoteApp/NoteApp/ProjectManager.cs
+++ NoteApp/NoteApp/ProjectManager.cs
@@ -1,21 +1,97 @@
 ﻿using System;
-using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace NoteApp
 {
     /// <summary>
-    /// Класс Менеджер Проекта. Содержит методы для сохранения и загрузки данных о всех записях.
+    /// Класс Менеджер Проекта. Содержит методы для сохранения и считывания данных о всех записях.
     /// </summary>
     public static class ProjectManager
     {
+        /// <summary>
+        /// Путь для сохранения файла.
+        /// </summary>
+        static string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)+ @"\json.txt";
         
+        /// <summary>
+        /// Метод для сохранения данных в файл.
+        /// </summary>
+        public static void SaveToFile(Project project)
+        {
+            
+            // С сайта https://www.newtonsoft.com/json/help/html/SerializeWithJsonSerializerToFile.htm + ctrl+c с занятия serialize
+            // serialize JSON to a string and then write string to a file
+            File.WriteAllText(path, JsonConvert.SerializeObject(project));
+
+            // serialize JSON directly to a file
+           using (StreamWriter file = File.CreateText(path))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.NullValueHandling = NullValueHandling.Include;
+                serializer.TypeNameHandling = TypeNameHandling.All;
+                serializer.Formatting = Formatting.Indented;
+                serializer.Serialize(file, project);
+            }
+
+            /* Работает, код из методички + ctrl+c с занятия serialize
+            //Создаём экземпляр сериализатора
+            JsonSerializer serializer = new JsonSerializer();
+            serializer.NullValueHandling = NullValueHandling.Include;
+            serializer.TypeNameHandling = TypeNameHandling.All;
+            serializer.Formatting = Formatting.Indented;
+            //Открываем поток для записи в файл с указанием пути
+            using (StreamWriter sw = new StreamWriter(path))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                //Вызываем сериализацию и передаем объект, который хотим сериализовать
+                serializer.Serialize(writer, project);
+            }
+            */
+        }
+
+
+        /// <summary>
+        /// Метод для считывания данных из файла. Если файл не найден, то возвращатся пустой экземпляр Project.
+        /// </summary>
+        public static Project ReturnToFile()
+        {
+            //Проверка существование файла
+           if (File.Exists(path))
+           {
+
+                // read file into a string and deserialize JSON to a type
+                Project project1 = JsonConvert.DeserializeObject<Project>(File.ReadAllText(path));
+
+                // deserialize JSON directly from a file
+                using (StreamReader file = File.OpenText(path))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    serializer.NullValueHandling = NullValueHandling.Include;
+                    serializer.TypeNameHandling = TypeNameHandling.All;
+                    serializer.Formatting = Formatting.Indented;
+                    Project project2 = (Project) serializer.Deserialize(file, typeof(Project));
+                }
+
+                string json = JsonConvert.SerializeObject(project1, Formatting.Indented);
+                Console.WriteLine(json);
+
+                return project1;
+            }
+           else
+           {
+               Project project = new Project();
+               return project;
+           }
+        }
+
+
     }
-    
+
 }
 
---
+++ NoteApp/NoteAppUI/MainForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NoteApp;
+
+namespace NoteAppUI
+{
+    public partial class MainForm : Form
+    {
+        private Project _project = new Project();
+
+       public MainForm()
+        {
+            InitializeComponent();
+            Note not = new Note();
+            not.Name = "Д1 — серия дизель-поездов, строившаяся ";
+            not.Text = "Всего было построено 605 составов. Конструкционно Д1 являлись усовершенствованной версией дизель-поездов серии Д и" +
+                       " отличались от последних прежде всего наличием более мощного дизельного двигателя, гидромеханической передачи и составностью," +
+                       " увеличенной на один прицепной вагон.";
+            not.Category = NoteCategory.People;
+            Note n2 = (Note)not.Clone();
+            n2.Name = "Svin";
+            Console.WriteLine(not.Name);
+            Console.WriteLine(n2.Name);
+            _project.Notes.Add(n2);
+            _project.Notes.Add(not);
+            //ProjectManager.SaveToFile(_project);
+            Console.WriteLine(n2.Name);
+            _project = ProjectManager.ReturnToFile();
+            
+        }
+
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+       
+    }
+
+}
